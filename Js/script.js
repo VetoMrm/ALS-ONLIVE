71f1504e3f9f1a7066b7a58edf8beaf212a09875
VetoMrm <manjunath.r@vetologic.com>
2020-07-16 16:21:29 +0530

    first commit

---
+++ Js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $("#bestmentor-slider").owlCarousel({
+        items: 4,
+        itemsDesktop: [1000, 3],
+        itemDesktopSmall: [979, 2],
+        itemTablet: [768, 2],
+        itemMobile: [650, 1],
+        pagination: true,
+        autoPlay: true,
+    })
+})
+
+$(document).ready(function() {
+    $("#happystudent-slider").owlCarousel({
+        items: 1,
+        itemsDesktop: [1000, 1],
+        itemDesktopSmall: [979, 1],
+        itemTablet: [768, 1],
+        itemMobile: [650, 1],
+        pagination: true,
+        autoPlay: true,
+    })
+})
